78e7e931ac9f05fe73204727d215dd7dc11193e6
shanoneCode <shanonedeffo@gmail.com>
2026-04-06 11:12:09 +0100

    first change

---
+++ src/main/java/com/monapp/App.java
@@ -0,0 +1,21 @@
+package com.monapp;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("  Application DevSecOps         ");
+        System.out.println("  Pipeline Jenkins + Trivy      ");
+        System.out.println("=================================");
+        System.out.println("Application démarrée avec succès !");
+    }
+
+    public static int additionner(int a, int b) {
+        return a + b;
+    }
+
+    public static String saluer(String nom) {
+        return "Bonjour " + nom + " !";
+    }
+
+}
